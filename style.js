5ae304f44b9b303f7e2e5b7a6720d3c145a6eb36
RaquelROES <32135998+RaquelROES@users.noreply.github.com>
2018-08-08 18:02:44 +0200

    Update style.js

--- style.js
+++ style.js
@@ -33,14 +33,11 @@
 				{ name: 'Celda 1', element: 'td', attributes: { 'class': 'bck-td' } },
 				{ name: 'Celda 2', element: 'td', attributes: { 'class': 'bck-td2' } },
 				
-				{ name: 'Icono Ordenador', element: 'span', attributes: { 'class': 'icon icon-ordenador' } },
+				{ name: 'Icono Exclamación', element: 'span', attributes: { 'class': 'icon icon-exclamacion' } },
+				{ name: 'Icono Lupa', element: 'span', attributes: { 'class': 'icon icon-lupa' } },
 				{ name: 'Icono Bombilla', element: 'span', attributes: { 'class': 'icon icon-bombilla' } },
-				{ name: 'Icono Química', element: 'span', attributes: { 'class': 'icon icon-quimica' } },
-				{ name: 'Icono Birrete', element: 'span', attributes: { 'class': 'icon icon-birrete' } },
-				{ name: 'Icono libro', element: 'span', attributes: { 'class': 'icon icon-libro' } },
-				{ name: 'Icono Lápiz', element: 'span', attributes: { 'class': 'icon icon-lapiz' } },
 
-				{ name: 'Caso verde', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-1' } },
+				{ name: 'Caja verde', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-1' } },
 				{ name: 'Caja azul', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-2' } },
 				{ name: 'Caja morada', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-3' } }
 			]
